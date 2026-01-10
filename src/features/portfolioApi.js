a43d5e5206b9f7b5312a1dd984e3042c723ae60d
Michał Kowalczyk <michalkowal3k@gmail.com>
2026-01-10 16:54:29 +0100

    movies-browser-react repo added

--- src/features/portfolioApi.js
+++ src/features/portfolioApi.js
@@ -3,6 +3,14 @@ import axios from "axios";
 export const githubAPIBaseURL = "https://api.github.com";
 export const githubUsername = "Hirazuku";
 
-export const getRepositories = username =>
-    axios.get(`${githubAPIBaseURL}/users/${username}/repos`)
-        .then(response => response.data);
+export const getRepositories = username => {
+  return Promise.all([
+    axios.get(`${githubAPIBaseURL}/users/${username}/repos`),
+    axios.get(`${githubAPIBaseURL}/repos/mg-wiewiora/movies-browser-react`)
+  ]).then(([reposRes, repoRes]) => {
+    return [
+      repoRes.data,
+      ...reposRes.data
+    ];
+  });
+};
